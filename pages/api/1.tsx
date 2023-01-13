b403f6e061d88f32987afa67b80992e1e1b0fd0e
Lucas Rau <lucasmrau@gmail.com>
2023-01-13 18:09:20 -0500

    commit

--- pages/api/1.tsx
+++ pages/api/1.tsx
@@ -7,8 +7,6 @@ export const config = {
   runtime: 'edge',
 };
 
-
-
 // Make sure the font exists in the specified path:
 const font = fetch(new URL('../../assets/Roboto-Bold.ttf', import.meta.url)).then(
   (res) => res.arrayBuffer(),
@@ -18,19 +16,25 @@ export default async function handler(req: NextRequest, res: NextResponse) {
   const fontData = await font;
 
   const { searchParams } = req.nextUrl;
-  const logoURL = searchParams.get('logoURL');
+  let logoURL = searchParams.get('logoURL');
   const title = searchParams.get('title');
   // const brandcolor = searchParams.get('brandcolor');
-  // const windowurl = searchParams.get('windowurl');
   const brandname = searchParams.get('brandname');
-  const bg = searchParams.get('bg');
+  let bg = searchParams.get('bg');
 
-  // if (!title) {
-  //   return new ImageResponse(<>Visit with &quot;?title=vercel&quot;</>, {
-  //     width: 1200,
-  //     height: 630,
-  //   });
-  // }
+  if(!bg) {
+    bg = '#202024'
+  }
+
+  function loadImg() {
+    if (logoURL) {
+      return <img
+        width="150"
+        height="150"
+        src={logoURL}
+      />
+    }
+  }
 
   return new ImageResponse(
     (
@@ -48,11 +52,9 @@ export default async function handler(req: NextRequest, res: NextResponse) {
           textAlign: 'center'
         }}
       >
-        <img
-          width="150"
-          height="150"
-          src={logoURL}
-        />
+
+        {loadImg()}
+
         <h1 style={{
           fontFamily: '"Roboto"',
           color: `${getTextColor(bg)}`,
@@ -63,6 +65,7 @@ export default async function handler(req: NextRequest, res: NextResponse) {
         }}>
           {title}
         </h1>
+        
         <p style={{
           fontFamily: '"Roboto"',
           color: `${getTextColor(bg)}`,
--- pages/api/2.tsx
+++ pages/api/2.tsx
@@ -1,33 +1,85 @@
 import { ImageResponse } from '@vercel/og';
+import { NextRequest, NextResponse } from 'next/server';
+
+import getTextColor from '../../utils/GetColor';
 
 export const config = {
   runtime: 'edge',
 };
 
-export default function handler(req, res) {
-    // const { test } = req.query
-        
-    return new ImageResponse(
-        (
-          <div
-            style={{
-              fontSize: 128,
-              background: 'black',
-              color: '#fff',
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              textAlign: 'center',
-              alignItems: 'center',
-              justifyContent: 'center'
-            }}
-          >      
-           Hello
-          </div>
-        ),
+/*
+GET https://api.screenshotone.com/take?url=https://www.saasbase.dev/collect-pre-sales-revenue-from-earlyadopters-
+using-the-stripe-api/&access_key=a8xutsSytmBvZQ
+*/
+
+// Make sure the font exists in the specified path:
+const font = fetch(new URL('../../assets/Roboto-Bold.ttf', import.meta.url)).then(
+  (res) => res.arrayBuffer(),
+);
+
+export default async function handler(req: NextRequest, res: NextResponse) {
+  const fontData = await font;
+
+  const { searchParams } = req.nextUrl;
+  //const brandcolor = searchParams.get('brandcolor');
+  const windowurl = searchParams.get('windowurl');
+  const brandname = searchParams.get('brandname');
+  let bg = searchParams.get('bg');
+
+  if(!bg) {
+    bg = '#202024'
+  }
+
+  const printimage = `https://api.screenshotone.com/take?url=${windowurl}/&access_key=${process.env.SCREENSHOTONE_KEY}`
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          background: bg,
+          width: '100%',
+          height: '100%',
+          padding: 10,
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          fontFamily: '"Roboto"',
+          textAlign: 'center',
+          position: 'relative'
+        }}
+      >
+        <h1 style={{
+          fontFamily: '"Roboto"',
+          color: `${getTextColor(bg)}`,
+          fontSize: 40,
+          paddingLeft: 50,
+          paddingRight: 50,
+          marginTop: 30
+        }}>
+          {brandname}
+        </h1>
+        <img
+          width="800"
+          height="500"
+          src={printimage}
+          style={{
+            position: 'absolute',
+            bottom: 0
+          }}
+        />
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 627,
+      fonts: [
         {
-          width: 1200,
-          height: 627,
+          name: 'Roboto',
+          data: fontData,
+          style: 'normal',
         },
-      );
-    }
+      ],
+    },
+  );
+}
