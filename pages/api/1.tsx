892f0ea74cba0b6c140394d80405a470b382e461
Lucas Rau <lucasmrau@gmail.com>
2023-01-13 19:46:46 -0500

    commit

--- pages/api/1.tsx
+++ pages/api/1.tsx
@@ -1,76 +1,85 @@
-import { ImageResponse } from '@vercel/og';
-import { NextRequest, NextResponse } from 'next/server';
+import { ImageResponse } from "@vercel/og";
+import { NextRequest, NextResponse } from "next/server";
 
-import getTextColor from '../../utils/GetColor';
+import getTextColor from "../../utils/GetColor";
 
+//Vercel og config
 export const config = {
-  runtime: 'edge',
+  runtime: "edge",
 };
 
-// Make sure the font exists in the specified path:
-const font = fetch(new URL('../../assets/Roboto-Bold.ttf', import.meta.url)).then(
-  (res) => res.arrayBuffer(),
-);
+// Example of imported font
+const font = fetch(
+  new URL("../../assets/Roboto-Bold.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
 
+//HTTP REQUEST
 export default async function handler(req: NextRequest, res: NextResponse) {
+  //Next.Js way of specifying HTTP request, if you really want to use a GET method: if (req.method === 'GET' ) {}
+
+  //import font
   const fontData = await font;
 
+  //variables from query params
   const { searchParams } = req.nextUrl;
-  let logoURL = searchParams.get('logoURL');
-  const title = searchParams.get('title');
-  // const brandcolor = searchParams.get('brandcolor');
-  const brandname = searchParams.get('brandname');
-  let bg = searchParams.get('bg');
+  let logoURL = searchParams.get("logoURL");
+  const title = searchParams.get("title");
+  //const brandcolor = searchParams.get('brandcolor'); if you want to add brand color
+  const brandname = searchParams.get("brandname");
+  let bg = searchParams.get("bg");
 
-  if(!bg) {
-    bg = '#202024'
+  //default background
+  if (!bg) {
+    bg = "#202024";
   }
 
+  //checking if there is a logo img
   function loadImg() {
     if (logoURL) {
-      return <img
-        width="150"
-        height="150"
-        src={logoURL}
-      />
+      return <img width="150" height="150" src={logoURL} />;
     }
   }
 
+  //OG image
   return new ImageResponse(
     (
       <div
         style={{
-          display: 'flex',
+          display: "flex",
           background: bg,
-          width: '100%',
-          height: '100%',
+          width: "100%",
+          height: "100%",
           padding: 10,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
           fontFamily: '"Roboto"',
-          textAlign: 'center'
+          textAlign: "center",
         }}
       >
-
+        {/* Loading img if available */}
         {loadImg()}
 
-        <h1 style={{
-          fontFamily: '"Roboto"',
-          color: `${getTextColor(bg)}`,
-          fontSize: 40,
-          paddingLeft: 50,
-          paddingRight: 50,
-          marginBottom: 0
-        }}>
+        <h1
+          style={{
+            fontFamily: '"Roboto"',
+            color: `${getTextColor(bg)}`,//the function to pick color dinamically is on utils folder
+            fontSize: 40,
+            paddingLeft: 50,
+            paddingRight: 50,
+            marginBottom: 0,
+          }}
+        >
           {title}
         </h1>
-        
-        <p style={{
-          fontFamily: '"Roboto"',
-          color: `${getTextColor(bg)}`,
-          fontSize: 20
-        }}>
+
+        <p
+          style={{
+            fontFamily: '"Roboto"',
+            color: `${getTextColor(bg)}`,
+            fontSize: 20,
+          }}
+        >
           {brandname}
         </p>
       </div>
@@ -80,11 +89,11 @@ export default async function handler(req: NextRequest, res: NextResponse) {
       height: 627,
       fonts: [
         {
-          name: 'Roboto',
+          name: "Roboto",
           data: fontData,
-          style: 'normal',
+          style: "normal",
         },
       ],
-    },
+    }
   );
 }
--- pages/api/2.tsx
+++ pages/api/2.tsx
@@ -7,31 +7,34 @@ export const config = {
   runtime: 'edge',
 };
 
-/*
-GET https://api.screenshotone.com/take?url=https://www.saasbase.dev/collect-pre-sales-revenue-from-earlyadopters-
-using-the-stripe-api/&access_key=a8xutsSytmBvZQ
-*/
-
-// Make sure the font exists in the specified path:
+// Example of imported font
 const font = fetch(new URL('../../assets/Roboto-Bold.ttf', import.meta.url)).then(
   (res) => res.arrayBuffer(),
 );
 
+//HTTP REQUEST
 export default async function handler(req: NextRequest, res: NextResponse) {
+   //Next.Js way of specifying HTTP request, if you really want to use a GET method: if (req.method === 'GET' ) {}
+
+  //import font
   const fontData = await font;
 
+  //variables from query params
   const { searchParams } = req.nextUrl;
-  //const brandcolor = searchParams.get('brandcolor');
+  //const brandcolor = searchParams.get('brandcolor'); if you want to add brand color
   const windowurl = searchParams.get('windowurl');
   const brandname = searchParams.get('brandname');
   let bg = searchParams.get('bg');
 
+  //default background
   if(!bg) {
     bg = '#202024'
   }
 
+  //Consuming API from screenshotone, please generate your access key and add to .env(SCREENSHOTONE_KEY)
   const printimage = `https://api.screenshotone.com/take?url=${windowurl}/&access_key=${process.env.SCREENSHOTONE_KEY}`
 
+  //OG image
   return new ImageResponse(
     (
       <div
@@ -51,7 +54,7 @@ export default async function handler(req: NextRequest, res: NextResponse) {
       >
         <h1 style={{
           fontFamily: '"Roboto"',
-          color: `${getTextColor(bg)}`,
+          color: `${getTextColor(bg)}`,//the function to pick color dinamically is on utils folder
           fontSize: 40,
           paddingLeft: 50,
           paddingRight: 50,
